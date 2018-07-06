27b879ed25fc73979f394e67090de86d746dcbf7
tiny656 <tiny656@hotmail.com>
2018-07-07 01:52:30 +0800

    add move delay feature

--- 2048/Assets/Scripts/GameManager.cs
+++ 2048/Assets/Scripts/GameManager.cs
@@ -15,11 +15,11 @@ public class GameManager : MonoBehaviour
     public GameObject MainPanel;
     public GameObject GameOverPanel;
 
-
-    private bool[] lineMoveComplete = new bool[MatrixSize];
     private const int MatrixSize = 4;
     private readonly Tile[,] allTile = new Tile[MatrixSize, MatrixSize];
     private readonly List<Tile> emptyTiles = new List<Tile>();
+    private bool isMoved;
+    private bool[] lineCompleted;
 
     void Start()
     {
@@ -33,6 +33,7 @@ public class GameManager : MonoBehaviour
         TryGenerateNewTile();
         this.gameStatus = GameStatus.Playing;
         this.GameOverPanel.SetActive(false);
+        Debug.Log("Game Manager Star Over");
     }
 
     public void NewGame()
@@ -40,34 +41,8 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
-
-
-    public void Move(MoveDirection moveDirection)
+    private void CheckGameOver()
     {
-        bool hasTileMoved = false;
-        Position startPosition = new Position();
-        for (int i = 0; i < MatrixSize; i++)
-        {
-            if (moveDirection == MoveDirection.Up || moveDirection == MoveDirection.Down)
-            {
-                startPosition.RowIndex = moveDirection == MoveDirection.Up ? 0 : MatrixSize - 1;
-                startPosition.ColIndex = i;
-            }
-            else
-            {
-                startPosition.RowIndex = i;
-                startPosition.ColIndex = moveDirection == MoveDirection.Left ? 0 : MatrixSize - 1;
-            }
-            hasTileMoved |= this.UpdateTiles(startPosition, moveDirection);
-        }
-
-        if (hasTileMoved)
-        {
-            TryGenerateNewTile();
-            TryGenerateNewTile();
-        }
-
-
         if (!this.CanMove() || this.IsReachGoal())
         {
             this.gameStatus = GameStatus.GameOver;
@@ -76,9 +51,11 @@ public class GameManager : MonoBehaviour
                 + string.Format("\nYou score\n{0}", ScoreManager.GetInstance().Score.ToString());
             this.GameOverPanel.transform.Find("GameOverText").GetComponent<Text>().text = gameOverText;
         }
-
+        else
+        {
+            this.gameStatus = GameStatus.Playing;
+        }
     }
-
 
     private void TryGenerateNewTile()
     {
@@ -114,80 +91,188 @@ public class GameManager : MonoBehaviour
         }
         return false;
     }
-
-    private Position FindNextNotZeroPosWithDirection(int pos, Position startPosition, MoveDirection moveDirection)
+    public void Move(MoveDirection moveDirection)
     {
-        Position curPosition = new Position();
-        Position nextNotZeroPos = new Position
+        Debug.Log(string.Format("Try Move {0}", moveDirection.ToString()));
+        this.isMoved = false;
+        this.gameStatus = GameStatus.WaitingForMoveToEnd;
+        if (this.Delay > 0f)
         {
-            RowIndex = -1,
-            ColIndex = -1
-        };
+            StartCoroutine(this.InternalMoveWithDelay(moveDirection));
+        }
+        else
+        {
+            this.InternalMove(moveDirection);
+        }
+    }
 
-        for (int i = pos + 1; i < MatrixSize; i += 1)
+    private void InternalMove(MoveDirection moveDirection)
+    {
+        Tile[] tileInLine;
+        int matrixSize = this.allTile.GetLength(0);
+        for (int i = 0; i < matrixSize; i++)
         {
             if (moveDirection == MoveDirection.Up || moveDirection == MoveDirection.Down)
-            {
-                curPosition.RowIndex = moveDirection == MoveDirection.Up ? i : MatrixSize -i - 1;
-                curPosition.ColIndex = startPosition.ColIndex;
-            }
-            else 
-            {
-                curPosition.RowIndex = startPosition.RowIndex;
-                curPosition.ColIndex = moveDirection == MoveDirection.Left ? i : MatrixSize - i - 1;
-            }
+                tileInLine = GetTilesFromOneColumn(i, moveDirection);
+            else
+                tileInLine = GetTilesFromOneRow(i, moveDirection);
+            this.UpdateTileOnOneLine(i, tileInLine);
+        }
+        if (this.isMoved)
+        {
+            this.TryGenerateNewTile();
+            this.TryGenerateNewTile();
+        }
+        this.CheckGameOver();
+    }
 
-            if (this.allTile[curPosition.RowIndex, curPosition.ColIndex].Number != 0)
+    private IEnumerator InternalMoveWithDelay(MoveDirection moveDirection)
+    {
+        Tile[] tileInLine;
+        int matrixSize = this.allTile.GetLength(0);
+        this.lineCompleted = new bool[matrixSize];
+        for (int i = 0; i < matrixSize; i++)
+        {
+            this.lineCompleted[i] = false;
+            if (moveDirection == MoveDirection.Up || moveDirection == MoveDirection.Down)
+                tileInLine = this.GetTilesFromOneColumn(i, moveDirection);
+            else
+                tileInLine = this.GetTilesFromOneRow(i, moveDirection);
+            StartCoroutine(this.UpdateTileOnOneLineWithDelay(i, tileInLine));
+        }
+        while (this.lineCompleted.Any(x => x == false))
+        {
+            Debug.Log("wait for coroutine");
+            yield return null;
+        }
+        if (this.isMoved)
+        {
+            this.TryGenerateNewTile();
+            this.TryGenerateNewTile();
+        }
+        this.CheckGameOver();
+    }
+
+    private void UpdateTileOnOneLine(int lineIndex, Tile[] tileInLine)
+    {
+        int size = tileInLine.Length;
+        for (int i = 0; i < size; i++)
+        {
+            var nextNotZeroPos = FindNextNonZeroPos(tileInLine, i + 1);
+            if (nextNotZeroPos == -1) continue;
+
+            Tile curTile = tileInLine[i];
+            Tile nextTile = tileInLine[nextNotZeroPos];
+            if (curTile.Number == 0)
+            {// Move tile
+                curTile.Number = nextTile.Number;
+                nextTile.Number = 0;
+                i -= 1;
+                this.emptyTiles.Remove(curTile);
+                this.emptyTiles.Add(nextTile);
+                this.isMoved = true;
+            }
+            else if (curTile.Number == nextTile.Number)
+            {// Merge tile
+                ScoreManager.GetInstance().Score += curTile.Number;
+                curTile.Number *= 2;
+                nextTile.Number = 0;
+                this.emptyTiles.Add(nextTile);
+                this.isMoved = true;
+            }
+        }
+    }
+
+    private IEnumerator UpdateTileOnOneLineWithDelay(int lineIndex, Tile[] tileInLine)
+    {
+        int size = tileInLine.Length;
+        Tile curTile, nextTile;
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size - 1; j++)
             {
-                nextNotZeroPos = curPosition;
+                curTile = tileInLine[j];
+                nextTile = tileInLine[j + 1];
+                if (curTile.Number == 0 && nextTile.Number != 0)
+                {
+                    curTile.Number = nextTile.Number;
+                    nextTile.Number = 0;
+                    this.emptyTiles.Add(nextTile);
+                    this.emptyTiles.Remove(curTile);
+                    this.isMoved = true;
+                    yield return new WaitForSeconds(this.Delay);
+                }
+            }
+        }
+        int pos = -1;
+        for (int i = 0; i < size - 1; i++)
+        {
+            curTile = tileInLine[i];
+            nextTile = tileInLine[i + 1];
+            if (curTile.Number != 0 && curTile.Number == nextTile.Number)
+            {
+                ScoreManager.GetInstance().Score += curTile.Number;
+                curTile.Number *= 2;
+                nextTile.Number = 0;
+                this.emptyTiles.Add(nextTile);
+                pos = i + 1;
+                this.isMoved = true;
+                yield return new WaitForSeconds(this.Delay);
                 break;
             }
         }
-        return nextNotZeroPos;
+        for (int i = pos; pos != -1 && i < size - 1; i++)
+        {
+            curTile = tileInLine[i];
+            nextTile = tileInLine[i + 1];
+            if (curTile.Number == 0 && nextTile.Number != 0)
+            {
+                curTile.Number = nextTile.Number;
+                nextTile.Number = 0;
+                this.emptyTiles.Add(nextTile);
+                this.emptyTiles.Remove(curTile);
+                this.isMoved = true;
+                yield return new WaitForSeconds(this.Delay);
+            }
+        }
+        this.lineCompleted[lineIndex] = true;
     }
 
 
-    private bool UpdateTiles(Position startPosition, MoveDirection moveDirection)
+    private static int FindNextNonZeroPos(Tile[] tileInLine, int pos)
     {
-        bool result = false;
-        for (int i = 0; i < MatrixSize; i += 1)
+        for (int i = pos; i < tileInLine.Length; i++)
         {
-            var nextNotZeroPos = this.FindNextNotZeroPosWithDirection(i, startPosition, moveDirection);
+            if (tileInLine[i].Number != 0) return i;
+        }
+        return -1;
+    }
 
-            if (nextNotZeroPos.IsValidPosition())
-            {
-                // Get current tile and next not zero tile
-                Tile curTile, nextTile;
-                if (moveDirection == MoveDirection.Up || moveDirection == MoveDirection.Down)
-                {
-                    curTile = this.allTile[moveDirection == MoveDirection.Up ? i : MatrixSize - i - 1, startPosition.ColIndex];
-                    nextTile = this.allTile[nextNotZeroPos.RowIndex, startPosition.ColIndex];
-                }
-                else
-                {
-                    curTile = this.allTile[startPosition.RowIndex, moveDirection == MoveDirection.Left ? i : MatrixSize - i - 1];
-                    nextTile = this.allTile[startPosition.RowIndex, nextNotZeroPos.ColIndex];
-                }
+    private Tile[] GetTilesFromOneColumn(int pos, MoveDirection moveDirection)
+    {
+        int matrixSize = this.allTile.GetLength(0);
+        Tile[] result = new Tile[matrixSize];
+        for (int lineIndex = 0; lineIndex < matrixSize; lineIndex++)
+        {
+            if (moveDirection == MoveDirection.Up)
+                result[lineIndex] = this.allTile[lineIndex, pos];
+            else
+                result[lineIndex] = this.allTile[matrixSize - lineIndex - 1, pos];
+        }
+        return result;
+    }
 
-                // Update current tile and next not zero tile
-                if (curTile.Number == 0)
-                {// Move tile
-                    curTile.Number = nextTile.Number;
-                    nextTile.Number = 0;
-                    i -= 1;
-                    this.emptyTiles.Remove(curTile);
-                    this.emptyTiles.Add(nextTile);
-                    result = true;
-                }
-                else if (curTile.Number == nextTile.Number)
-                {// Merge tile
-                    ScoreManager.GetInstance().Score += curTile.Number;   
-                    curTile.Number *= 2;
-                    nextTile.Number = 0;
-                    this.emptyTiles.Add(nextTile);
-                    result = true;
-                }
-            }
+    private Tile[] GetTilesFromOneRow(int pos, MoveDirection moveDirection)
+    {
+        int matrixSize = this.allTile.GetLength(0);
+        Tile[] result = new Tile[matrixSize];
+        for (int lineIndex = 0; lineIndex < matrixSize; lineIndex++)
+        {
+            if (moveDirection == MoveDirection.Left)
+                result[lineIndex] = this.allTile[pos, lineIndex];
+            else
+                result[lineIndex] = this.allTile[pos, matrixSize - lineIndex - 1];
         }
         return result;
     }
